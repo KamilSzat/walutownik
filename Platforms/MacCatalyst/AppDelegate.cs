130effd0c7953b6d621390860a32c41747631d33
KamilSzat <uT_3776@zsp3zamosc.pl>
2024-10-31 08:44:30 +0100

    poprawiony kod z walutami

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace walutownik
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
